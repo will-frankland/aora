bc5a483831c383c3b5c4e9453382ffe84d7765a4
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-16 10:05:29 -0400

    style latest videos with zoomIn/out

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -13,13 +13,14 @@ import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import { getAllPosts } from "../../lib/appwrite";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
 
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
   const [refreshing, setRefreshing] = useState(false);
 
@@ -28,8 +29,6 @@ const Home = () => {
     await refetch();
     setRefreshing(false);
   };
-
-  console.log(posts)
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
@@ -62,7 +61,7 @@ const Home = () => {
               <Text className="text-gray-100 text-lg font-pregular mb-3">
                 Latest Videos
               </Text>
-              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
