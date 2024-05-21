6a39649566aef9aa9c8e5f665de1a110e40ec95a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-21 14:03:41 -0400

    create styling for create page, no logic

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -16,9 +16,10 @@ import EmptyState from "../../components/EmptyState";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
-
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
 
@@ -42,10 +43,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome Back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Will_
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
