b389bae3a15a58b27ca93f01879678a634bd76a3
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-21 12:07:22 -0400

    setup InfoBox component with props

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-const InfoBox = () => {
+const InfoBox = ({ title, subtitle, containerStyles, titleStyles }) => {
   return (
-    <View>
-      <Text>InfoBox</Text>
+    <View className={containerStyles}>
+      <Text className={`text-white text-center font-psemibold ${titleStyles}`}>
+        {title}
+      </Text>
+      <Text className="text-sm text-gray-100 text-center font-pregular">
+        {subtitle}
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default InfoBox
+export default InfoBox;
