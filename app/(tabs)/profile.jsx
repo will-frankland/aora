dc5dc4848910ff38d93c07168256aafe6e18521b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-21 12:05:02 -0400

    setup profile initial layout

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,29 +1,22 @@
-import {
-  View,
-  Text,
-  FlatList,
-} from "react-native";
-import React, { useState, useEffect } from "react";
+import { View, FlatList, TouchableOpacity, Image } from "react-native";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import SearchInput from "../../components/SearchInput";
 import EmptyState from "../../components/EmptyState";
-import { searchPosts } from "../../lib/appwrite";
+import { getUserPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
-import { useLocalSearchParams } from "expo-router";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { icons } from "../../constants";
+import InfoBox from "../../components/InfoBox";
 
 const Profile = () => {
-  const { query } = useLocalSearchParams();
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
 
-  const { data: posts, refetch } = useAppwrite(
-    () => searchPosts(query));
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
 
-  const [refreshing, setRefreshing] = useState(false);
-
-  useEffect(() => {
-    refetch();
-  }, [query]);
+  const logout = () => {};
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -32,13 +25,41 @@ const Profile = () => {
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
-          <View className="my-6 px-4">
-            <Text className="font-pmedium text-sm text-gray-100">
-              Search Results
-            </Text>
-            <Text className="text-2xl font-psemibold text-white">{query}</Text>
-            <View className="mt-6 mb-8">
-              <SearchInput initialQuery={query} />
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logout}
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
+            </TouchableOpacity>
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+            <View className="mt-5 flex-row">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mr-10"
+                titleStyles="text-lg"
+              />
+              <InfoBox
+                title="1.2k"
+                subtitle="Followers"
+                titleStyles="text-xl"
+              />
             </View>
           </View>
         )}
